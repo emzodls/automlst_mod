579742165ea1a0c8a7a258743459307fca2ec8be
Lab user <labuser@ZiemertAntismash.local>
2018-02-12 13:43:54 +0100

    fixed Step 3 not showing orgs to remove on load; fixed amount of genes loading on reset

--- webapp/app/static/js/step3.js
+++ webapp/app/static/js/step3.js
@@ -1,46 +1,3 @@
-function geneError(xhr,ajaxOptions,thrownError) { // communication error
-console.log(xhr,ajaxOptions,thrownError);
-}
-
-function geneSuccess(data,textStatus,xhr) {
-    var counter;
-    var mlstGenes = data;
-    /*var mlstGenes = data["genes"];
-    var selectedGenes = data["selected"];*/
-    for (counter=0; counter<mlstGenes.length; counter++) {
-        var geneInfo = mlstGenes[counter];
-        console.log(geneInfo);
-        $('#mlstsel').append("<option id='" + geneInfo.acc + "' data-title='"+ geneInfo.name +": "+geneInfo.desc+"' value='"+geneInfo.acc+"' data-del='"+geneInfo.orgdel +"'>" + geneInfo.name +": "+geneInfo.desc+"</option>");
-        }
-     if (data["selected"]) { //currently an useless check
-        loadSelected('#mlstsel','#mlstlist',selectedGenes);
-     } else {
-        loadDefaults('#mlstsel','#mlstlist',100);
-    }
-    /*if (data["deletedorgs"]) {
-        var counter2;
-        for (counter2=0;counter2<deletedOrgs.length;counter2++){
-            var delInfo = deletedOrgs[counter2];
-            $('#todelete').append("<li>"+delInfo.orgname+"</li>");
-        }
-        $('#deletewarning').removeClass("hidden");
-    } */
-}
-
-function getGenes() {
-var jobid = $('#jobinfo').val();
-$.ajax({
-        // Your server script to process the upload
-        contentType: 'application/json',
-        url: '/results/'+jobid+'/step3/genes',
-        async: true,
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: geneSuccess,
-        error: geneError});
-
-}
 // not elegant but at least it works
 function findDeleted() {
 var deletedList = [];
@@ -67,8 +24,54 @@ if (deletedList.length > 0) {
 }
 }
 
+function geneError(xhr,ajaxOptions,thrownError) { // communication error
+console.log(xhr,ajaxOptions,thrownError);
+}
+
+function geneSuccess(data,textStatus,xhr) {
+    var counter;
+    var mlstGenes = data;
+    /*var mlstGenes = data["genes"];
+    var selectedGenes = data["selected"];*/
+    for (counter=0; counter<mlstGenes.length; counter++) {
+        var geneInfo = mlstGenes[counter];
+        console.log(geneInfo);
+        $('#mlstsel').append("<option id='" + geneInfo.acc + "' data-title='"+ geneInfo.name +": "+geneInfo.desc+"' value='"+geneInfo.acc+"' data-del='"+geneInfo.orgdel +"'>" + geneInfo.name +": "+geneInfo.desc+"</option>");
+        }
+     if (data["selected"]) { //currently an useless check
+        loadSelected('#mlstsel','#mlstlist',selectedGenes);
+     } else {
+        loadDefaults('#mlstsel','#mlstlist',100);
+    }
+    findDeleted();
+    /*if (data["deletedorgs"]) {
+        var counter2;
+        for (counter2=0;counter2<deletedOrgs.length;counter2++){
+            var delInfo = deletedOrgs[counter2];
+            $('#todelete').append("<li>"+delInfo.orgname+"</li>");
+        }
+        $('#deletewarning').removeClass("hidden");
+    } */
+}
+
+function getGenes() {
+var jobid = $('#jobinfo').val();
+$.ajax({
+        // Your server script to process the upload
+        contentType: 'application/json',
+        url: '/results/'+jobid+'/step3/genes',
+        async: true,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: geneSuccess,
+        error: geneError});
+
+}
+
+
 function validateForm() {
-if ($('.selectablein').has('option').length>0) {
+if ($('.selectablein').has('option').length>10) {
 return "validated";
 } else {
 return "notvalidated";}
